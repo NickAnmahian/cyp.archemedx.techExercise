f2b2ed3a188cc85c903c316c8a15332a21cc17fc
Nick Anmahian <nza0121@gmail.com>
2024-05-13 01:53:25 -0400

    make activity_isolationOff_spec to test having multiple its, success!

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,12 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    testIsolation: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      
     },
   },
+  
   defaultCommandTimeout: 10000 // 10 seconds in milliseconds
 });
